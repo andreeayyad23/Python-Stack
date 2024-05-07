4efdca6c7ade2f8350188e7ed31ded62c0289d41
Andreh,ayyad <andreh,ayad@gmail.com>
2024-05-07 16:06:29 +0300

    dofo_fruits

---
+++ flask/flask_fundamentals/dojo_fruit_store/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)  
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    firstname = request.form["first_name"]
+    lastname = request.form["last_name"]
+    studentid = request.form ["student_id"]
+    apple = request.form["apple"]
+    strawberry = request.form["strawberry"]
+    raspberry = request.form["raspberry"]
+    total = int(apple)+int(strawberry)+int(raspberry)
+    print (request.form)
+    print (f"Charging,{firstname} {lastname} for {total} fruits")
+    return render_template("checkout.html",firstname = firstname ,lastname=("lastname"), studentid=studentid, 
+                           apple=int(apple),strawberry=int(strawberry),raspberry=int(raspberry), total=int(total))
+
+
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html")
+
+if __name__=="__main__":   
+    app.run(debug=True)    
