6e195f40ba7529ac3c02eb060cb5ffbbbd52b011
Andreh,ayyad <andreh,ayad@gmail.com>
2024-05-08 14:45:33 +0300

    First Django Project

---
+++ django/django_fundamentals/First_Django_Project/app_one/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path ,include
+from . import views
+urlpatterns = [
+     path('blogs', views.index),
+     path('',views.root),
+     path('blogs/new', views.new),
+     path('blogs/create',views.create),
+     path('blogs/<int:number>',views.show),
+     path('blogs/<int:number>/edit',views.edit),
+     path('blogs/<int:number>/delete',views.destroy)
+]
