443794f09e3588c98994189b70a28c95df80b2cc
Andreh,ayyad <andreh,ayad@gmail.com>
2024-05-12 22:12:26 +0300

    Great Number

---
+++ django/django_fundamentals/Great_Number_Game/Number_game/views.py
@@ -0,0 +1,33 @@
+
+
+from django.shortcuts import render, redirect
+import random
+
+def index(request):
+    if 'number' not in request.session:
+        num = random.randint(1, 100)
+        request.session['number'] = num
+        
+    return render(request, 'index.html')
+
+def process(request):
+    request.session['guess_number'] = request.POST['guess']
+
+    if int(request.session['guess_number']) == int(request.session['number']):
+        return redirect('/correct')
+
+    elif int(request.session['guess_number']) > int(request.session['number']):
+        return redirect('/too_high')
+
+    elif int(request.session['guess_number']) < int(request.session['number']):
+        return redirect('/too_low')
+
+def correct(request):
+
+    return render(request, 'correct.html')
+
+def too_high(request):
+    return render(request, 'too_high.html')
+
+def too_low(request):
+    return render(request, 'too_low.html')
