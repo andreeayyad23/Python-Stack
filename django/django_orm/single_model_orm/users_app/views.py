bacaa5fc3fc86227af5ec38af3709001a5809291
Andreh,ayyad <andreh,ayad@gmail.com>
2024-05-14 15:56:28 +0300

    Users with templates

--- django/django_orm/single_model_orm/users_app/views.py
+++ django/django_orm/single_model_orm/users_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Users
+def index(request):
+    data={
+        "users": Users.objects.all()
+    }
+    return render(request, 'index.html',data)
 
-# Create your views here.
+def create(request):
+    if request.method == 'POST':
+        adduser= Users.objects.create(
+            first_name=request.POST['firstname'],
+            last_name=request.POST['lastname'],
+            email_address=request.POST['email'],
+            age=request.POST['age'],
+        )
+        adduser.save()
+    return redirect('/')
