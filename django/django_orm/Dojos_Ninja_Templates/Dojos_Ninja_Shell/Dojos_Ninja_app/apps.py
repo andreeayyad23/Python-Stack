0934e82fc4fc3d8a9080de027035b6662b179e43
Andreh,ayyad <andreh,ayad@gmail.com>
2024-05-15 20:38:33 +0300

    Ninja and dojos templates

---
+++ django/django_orm/Dojos_Ninja_Templates/Dojos_Ninja_Shell/Dojos_Ninja_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DojosNinjaAppConfig(AppConfig):
+    name = 'Dojos_Ninja_app'
--- django/django_orm/Users_with_Templates/User_with_templates_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserWithTemplatesAppConfig(AppConfig):
-    name = 'User_with_templates_app'
