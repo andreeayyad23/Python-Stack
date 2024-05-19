ba315b630007caf0b9092b8fbae3caa06a85027e
Andreh,ayyad <andreh,ayad@gmail.com>
2024-05-19 23:46:33 +0300

    SemiRestful_Tv Shows validated

--- django/django_fullstack/Semi_Restful_TV_Shows/Semi_Restful_Shows_app/models.py
+++ django/django_fullstack/Semi_Restful_TV_Shows/Semi_Restful_Shows_app/models.py
@@ -1,4 +1,36 @@
+from copy import error
+import datetime
 from django.db import models
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Title cannot be less than 2 characters!"
+        else:
+            try:
+                show = Show.objects.get(title__iexact=postData['title'])
+                errors['title'] = "is taken please try another one"
+            except:
+                pass
+
+        if len(postData['network']) < 3:
+            errors["network"] = "Network should be at least 3 characters"
+
+        if 0 < len(postData['description']) and  len(postData['description']) < 10:
+            errors["description"] = "Description should be at least 10 characters"
+
+        if len(postData['release_date']) > 0:
+            _releaseDate = datetime.datetime.strptime(
+                postData['release_date'], '%Y-%m-%d')
+            _todayDate = datetime.datetime.today()
+            if _todayDate < _releaseDate:
+                errors['release_date'] = "date should be in the past!"
+        else:
+            errors['release_date'] = "Field is required!"
+
+        return errors
+
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
@@ -7,3 +39,4 @@ class Show(models.Model):
     description = models.TextField(default="No description is set.")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects= ShowManager()
--- django/django_fullstack/Semi_Restful_TV_Shows/Semi_Restful_Shows_app/views.py
+++ django/django_fullstack/Semi_Restful_TV_Shows/Semi_Restful_Shows_app/views.py
@@ -1,29 +1,38 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import *
 
+
 def index(request):
-    return redirect('/shows')
+    return redirect('shows/')
 
 def showsAll(request):
-    data={
+    context={
         'shows':Show.objects.all(),
     }
-    return render(request, 'read_all.html', data)
+    return render(request, 'read_all.html', context)
 
 def go_create(request):
     return render(request, 'create.html')
 
 def create_show (request):
     if request.method=="POST":
-        newShow= Show.objects.create(
-            title= request.POST['title'],
-            network=request.POST['network'],
-            release_date=request.POST['release_date'],
-            description=request.POST['description']
-        )
-        newShow.save()
-        return redirect(f'/shows/{newShow.id}')
-    return redirect('/')
+        errors = Show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+        else:
+            newShow= Show.objects.create(
+                title= request.POST['title'],
+                network=request.POST['network'],
+                release_date=request.POST['release_date'],
+                description=request.POST['description']
+            )
+            newShow.save()
+            messages.success(request, "Show successfully Added!")
+            return redirect(f'/shows/{newShow.id}')
+    return redirect('/shows/new')
+
 
 def readOne(request, _id):
     context={
@@ -39,13 +48,23 @@ def go_edit(request,_id):
 
 def edit_show(request,_id):
     if request.method=='POST':
-        edit_show= Show.objects.get(id=_id)
-        edit_show.title= request.POST['title']
-        edit_show.network=request.POST['network']
-        edit_show.release_date=request.POST['release_date']
-        edit_show.description=request.POST['description']
-        edit_show.save()
-    return redirect(f'/shows/{_id}')
+        errors = Show.objects.basic_validator(request.POST)
+        if len(errors) > 0 :
+            for key, value in errors.items():
+                messages.error(request, value)
+
+        else:
+            edit_show= Show.objects.get(id=_id)
+            edit_show.title= request.POST['title']
+            edit_show.network=request.POST['network']
+            edit_show.release_date=request.POST['release_date']
+            edit_show.description=request.POST['description']
+            edit_show.save()
+            messages.success(request, "Show successfully Update!")
+            return redirect(f'/shows/{_id}')
+    return redirect(f'/shows/{_id}/edit')
+
+
 
 
 def delete_show(request, _id):
